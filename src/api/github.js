c4b05a10620744ff1c81082a85e0eb378bf59c64
Alemarnery <aleemar.95@gmail.com>
2021-07-12 13:29:42 -0500

    first commit

---
+++ src/api/github.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const token = "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";
+const username = "alemarnery";
+
+const url = `${username}:${token}  https://api.github.com/user`;
