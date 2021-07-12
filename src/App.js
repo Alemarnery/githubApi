db2d0c7760df9619416e50ef6ffc1e9b791ba935
Alemarnery <aleemar.95@gmail.com>
2021-07-12 14:45:01 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -16,18 +16,38 @@ function App() {
     }
   }, [commits]);
 
-  console.log(commits);
-
   return (
-    <div className="App">
-      <ul class="list-group">
-        {commits.map(({ sha, commit }) => {
+    <div className="container">
+      <div class="jumbotron">
+        <h1 class="display-4">Alemarnery Villalobos</h1>
+        <p class="lead">
+          This project was created as part of a challenge from Fulltime Force by
+          Alemarnery Villalobos
+        </p>
+        <hr class="my-4" />
+      </div>
+
+      <ul className="list-group">
+        <p>
+          Commits from respository:
+          <a href="https://github.com/Alemarnery/githubApi" target="_blank">
+            GithubApi
+          </a>
+        </p>
+
+        {commits.map(({ sha, commit, author }) => {
           return (
-            <li class="list-group-item" key={sha}>
+            <li className="list-group-item" key={sha}>
               <div>
                 <h3>{commit.message}</h3>
               </div>
               <div>
+                <img
+                  alt="Avatar"
+                  src={author.avatar_url}
+                  className="rounded"
+                  style={{ width: 20, height: 20, marginRight: 10 }}
+                />
                 {commit.committer.name} committed{" "}
                 {moment(commit.committer.date).fromNow()}
               </div>
